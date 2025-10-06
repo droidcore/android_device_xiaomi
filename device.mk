c2837ee2b9889cf9eec44b888f2e5cba6156903d
Guidix <guidixm@gmail.com>
2025-10-06 14:41:15 +0300

    peridot: Build External Parts App
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -19,6 +19,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # pKVM
 $(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
 
+# Parts
+$(call inherit-product, packages/apps/XiaomiParts/parts.mk)
+
 # Qualcomm
 $(call soong_config_set,rfs,mpss_firmware_symlink_target,modem_firmware)
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
@@ -344,8 +347,10 @@ PRODUCT_PACKAGES += \
     NcmTetheringOverlay
 
 # Parts
-PRODUCT_PACKAGES += \
-    XiaomiParts
+include packages/apps/XiaomiParts/parts.mk
+TARGET_USE_CLEARSPEAKER := true
+TARGET_USE_SATURATIONSLIDER := true
+TARGET_USE_DCDIMMING := true
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
